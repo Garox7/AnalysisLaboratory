e1dcf6ab56baf0f39475fa6622a67533682ecbd3
Garox7 <giuseppe.garozzo2@icloud.com>
2023-10-17 10:54:46 +0200

    Laboratory methods

--- LaboratoryLibrary/Classes/Laboratory.cs
+++ LaboratoryLibrary/Classes/Laboratory.cs
@@ -2,22 +2,37 @@
 
 public class Laboratory
 {
-    public List<Analysis> Analysis {get; set;}
+    public Dictionary<string, Analysis> Analysis {get; set;}
     public List<Reagent> Reagents {get; set;}
 
-    public Laboratory() {
-        // TODO:
+    public Laboratory() {}
+
+    public void LoadAnalysesFromFile(string filePath)
+    {
+        // Implementa il caricamento delle analisi dal file ANALISI.DAT
+        // Aggiungi analisi e reagenti alla lista analyses e reagents
     }
 
-    public (ListOfAnalysis, ListOfReagents) getListAnalysisAndReagents()
+    public void LoadReagentsFromFile(string filePath)
     {
-        // TODO:
+        // Implementa il caricamento dei reagenti dal file MAGAZZINO.DAT
+        // Aggiorna le quantità disponibili dei reagenti esistenti e aggiungi nuovi reagenti se necessario
+    }
+
+    public void getListAnalysisAndReagents()
+    {
+        // Ottengo la lista delle analisi e dei loro reagenti richiesti.
+        // oppure stampo la lisa delle analisi e dei reagenti separatamente?
     }
 
     public void AnalysisPrenotation() 
     {
-        /*
-            Tale metodo deve contenere boh
-        */
+        // Tale funzione deve controllare la disponibilità dei reagenti richiesti per effettuare
+        // l'esame e aggiornare l'elenco. Nel caso dei reagenti non disponibili informa l'utente e non permette la prenotazione
+    }
+
+    public Reagent GetReagentWithMostAvailability()
+    {
+        // Restituisci il reagente con la maggiore disponibilità
     }
 }
