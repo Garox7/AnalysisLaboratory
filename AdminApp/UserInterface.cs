a016f9cd87d0a431e5d7150502a9258d757709bd
MartinaBenty <bentivegnamartina@gmail.com->
2023-10-20 16:43:27 +0200

    Create the user interface and Readme

--- AdminApp/UserInterface.cs
+++ AdminApp/UserInterface.cs
@@ -1,7 +1,27 @@
+using LaboratoryLibrary.Classes;
+// si deve fare la reference per provarlo.
 public class UserInterface
 {
     public static void User()
     {
         Console.WriteLine("Hello User");
+        Console.WriteLine("1. Book an analysis");
+        Console.WriteLine("2. View the history of reservation");
+        string choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                {
+                    ReservationAnalysis();
+                    break;
+                }
+            case "2":
+                {
+                    GetHistoryReservation();
+                    break;
+                }
+        }
+
+
     }
 }
