b08936d60426d39b08334e387e9154feaf21d10c
Garox7 <giuseppe.garozzo2@icloud.com>
2023-10-23 10:18:21 +0200

    fix

--- AdminApp/AdminProgram.cs
+++ AdminApp/AdminProgram.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using Newtonsoft.Json;
-using LaboratoryLibrary.Classes;
-using System.Diagnostics;
-using System.Transactions;
+﻿using System.Diagnostics;
 
 class Program
 {
@@ -40,62 +36,14 @@ class Program
     {
         Process.Start(@"C:\Users\Huawei\OneDrive\Desktop\esercizi\Martina\AnalysisLaboratory\UserApp\bin\Debug\net7.0\UserApp.exe");
     }
-
-    // public void Inizialization()
-    // {
-    //     // REAGENTS
-    //     Reagent reagent1 = new("reagente1");
-    //     Reagent reagent2 = new("reagente2");
-    //     Reagent reagent3 = new("reagente3");
-    //     Reagent reagent4 = new("reagente4");
-    //     Reagent reagent5 = new("reagente5");
-    //     Reagent reagent6 = new("reagente6");
-    //     Reagent reagent7 = new("reagente7");
-    //     Reagent reagent8 = new("reagente8");
-    //     Reagent reagent9 = new("reagente9");
-
-    //     Laboratory.Reagents.Add(reagent1);
-    //     Laboratory.Reagents.Add(reagent2);
-    //     Laboratory.Reagents.Add(reagent3);
-    //     Laboratory.Reagents.Add(reagent4);
-    //     Laboratory.Reagents.Add(reagent5);
-    //     Laboratory.Reagents.Add(reagent6);
-    //     Laboratory.Reagents.Add(reagent7);
-    //     Laboratory.Reagents.Add(reagent8);
-    //     Laboratory.Reagents.Add(reagent9);
-
-    //     // ANALYSIS
-    //     var array1 = new List<string>{"reagente9", "reagente7", "reagente6"};
-    //     var array2 = new List<string>{"reagente4", "reagente7", "reagente6"};
-    //     var array3 = new List<string>{"reagente3", "reagente2", "reagente1"};
-    //     var array4 = new List<string>{"reagente9", "reagente8", "reagente5"};
-    //     Analysis analysis1 = new("analisi_1", array1) ;
-    //     Analysis analysis2 = new("analisi_2", array2);
-    //     Analysis analysis3 = new("analisi_3", array3);
-    //     Analysis analysis4 = new("analisi_4", array4);
-
-    //     Laboratory.Analysis.Add(analysis1);
-    //     Laboratory.Analysis.Add(analysis2);
-    //     Laboratory.Analysis.Add(analysis3);
-    //     Laboratory.Analysis.Add(analysis4);
-
-    //     // WRITE FILE
-    //     JsonFileManager jsonFile = new();
-
-    //     string filePathAnalysis = "/Users/giuseppegarozzo/Desktop/FMF/AnalysisLaboratory/LaboratoryLibrary/Data/Analysis.JSON";
-    //     string filePathReagents = "/Users/giuseppegarozzo/Desktop/FMF/AnalysisLaboratory/LaboratoryLibrary/Data/Reagent.JSON";
-    //     jsonFile.WriteJsonFile(filePathAnalysis, Laboratory.Analysis, JsonConvert.SerializeObject);
-    //     jsonFile.WriteJsonFile(filePathReagents, Laboratory.Reagents, JsonConvert.SerializeObject);
-    // }
 }
 
 /*
 TODO:
-1. Classe prenotazione, V
-2. Creare Dictonary,
-3. Provare a serializzare e deserializzare i dati,
-4. Effettuare prenotazione sia lato Admin che User,
-5. Lato Admin ottenere per ogni Analisi gli utenti prenotati,
-6. Lato User ottenere per lo User corrente lo storico degli esami effettuati.
-7. 
+1. Sitemare Login
+2. Sistemare prenotazioni User
+3. Serializzare magazzino
+4. Check generico
+5. Controllo nome variabili (soprattutto private!!!!!)
+6. qualche test? chene dici 
 */
