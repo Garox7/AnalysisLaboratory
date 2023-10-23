ed393525d5c0ecc538239e4d5ef524d717000edd
MartinaBenty <bentivegnamartina@gmail.com->
2023-10-23 09:43:16 +0200

    add Authentication

---
+++ LaboratoryLibrary/Classes/Authentication.cs
@@ -0,0 +1,29 @@
+public interface IAuthenticator
+{
+    UserAuthenticationResult Autenticate (string username, string password);
+}
+
+public enum UserAuthenticationResult
+{
+    Admin,
+    User,
+    InvalidCredentials,
+}
+
+public class AutenticationManager:IAuthenticator
+{
+    public UserAuthenticationResult Autenticate (string username, string password)
+    {
+        if(username == "admin" && password=="adminpassword")
+        {
+            return UserAuthenticationResult.Admin;
+        }
+        else if (username=="user" && password =="userpassword")
+        {
+            return UserAuthenticationResult.User;
+        }
+        else{
+            return UserAuthenticationResult.InvalidCredentials;
+        }
+    }
+}
